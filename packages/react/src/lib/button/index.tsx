ef3428d0d6faf9f459c4270e15cee223051df086
lucystwt <lucy-stwt@outlook.com>
2022-10-17 04:26:13 +0800

    style: Button 样式调整

--- packages/react/src/lib/button/index.tsx
+++ packages/react/src/lib/button/index.tsx
@@ -10,9 +10,9 @@ export type ButtonProps = {
 }
 
 const StyledButton = styled.button<ButtonProps>`
+  background-color: transparent;
   padding: 7px 15px;
   border: none;
-  background-color: transparent;
   border-radius: ${themeVars.round};
   cursor: pointer;
   transition: color, background-color, border-color cubic-bezier(0.4, 0, 0.2, 1) 150ms;
@@ -20,42 +20,50 @@ const StyledButton = styled.button<ButtonProps>`
   ${({ variant }) =>
     variant === 'outlined' &&
     css`
-      padding: 6px 14px;
       background-color: ${themeVars.white};
+      padding: 6px 14px;
       border: 1px solid ${themeVars.border};
+
       &:hover,
       &:focus {
         color: ${themeVars.primaryLighter};
         border-color: ${themeVars.primaryLighter};
       }
+
       &:active {
         color: ${themeVars.primaryDark};
         border-color: ${themeVars.primaryDark};
       }
     `};
+
   ${({ variant }) =>
     variant === 'contained' &&
     css`
       color: ${themeVars.white};
       background-color: ${themeVars.primary};
+
       &:hover,
       &:focus {
         background-color: ${themeVars.primaryDark};
         border-color: ${themeVars.primaryDark};
       }
+
       &:active {
         background-color: ${themeVars.primaryDarkest};
         border-color: ${themeVars.primaryDarkest};
       }
     `}
+
   ${({ variant }) =>
     variant === 'text' &&
     css`
       color: ${themeVars.primary};
+
       &:hover,
       &:focus {
         color: ${themeVars.primaryDark};
       }
+
       &:active {
         color: ${themeVars.primaryDarkest};
       }
